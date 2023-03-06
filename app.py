07e586bbd75486b81defa092c7304e9cea222052
pedro berecibar <pedroberecibar.dev@gmail.com>
2023-03-06 15:39:39 +0100

    ver06032023-1

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    modify = db.Column(db.Boolean)
+
+    
+
+@app.route('/')
+def index():
+    todo_list= Todo.query.all()
+    return render_template('index.html', todo_list = todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    #add new item
+    n_title = request.form.get("title")
+    n_date = (request.form.get("date"))
+    n_time = request.form.get("time")
+    new_todo = Todo(title=n_title, complete=False, date=datetime.strptime(n_date, "%Y-%m-%d"), time=datetime.strptime(n_time, "%H:%M").time(), modify=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    #update new item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    #delete item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/modify/<int:todo_id>")
+def modify(todo_id):
+    #modify item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.modify = not todo.modify
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/acept-modify/<int:todo_id>",methods=["POST"])
+def aceptModify(todo_id):
+    #acept modify item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.title = request.form.get("title2")
+    db.session.commit()
+    modify(todo_id)
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
